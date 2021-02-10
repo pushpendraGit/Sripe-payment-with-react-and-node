4f0a2a90b2102f960a40aaa15450f9d97fbaa1e4
PushpendraMentora <pushpendra@mentora.institute>
2021-02-11 00:03:28 +0530

    stripe payment with react and node

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import StripeCheckout from 'react-stripe-checkout';
+import { useState } from 'react';
+
 
 function App() {
+  const [product, setProduct] = useState({
+    name: 'Love',
+    price: 10,
+    productBy: 'Pushpendra'
+  })
+
+  const makePayment = token => {
+    const body = {
+      token,
+      product
+    }
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+
+    return fetch(`http://localhost:8282/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+      
+    }).then(response => {
+       console.log('RESPONCE', response)
+     }).catch(error => console.log(error))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    
+       
+      <div className='payment-btn-cobtainer'>
+        <h5>Hii Payment By Pushpendra</h5>
+        {/* TODO: Put your public key in 'stripeKey' gentated by 'stripe' */}
+        <StripeCheckout
+          stripeKey= ''
+          token= {makePayment}
+          amount ={product.price *100}
+          name='By Love'
+        ><button className="btn-large pink">By Love in 2021</button></StripeCheckout>
+      </div>
     </div>
   );
 }
